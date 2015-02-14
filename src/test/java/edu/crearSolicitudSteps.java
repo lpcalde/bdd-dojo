751ce43fe9f0ad647acda3f41b10d9d9b41da39c
kleer <entrenamos@kleer.la>
2015-02-13 20:56:42 -0500

    Pasos para ATDD

--- src/test/java/edu/crearSolicitudSteps.java
+++ src/test/java/edu/crearSolicitudSteps.java
@@ -1,24 +1,43 @@
 package edu;
 
 import com.codeborne.selenide.Selenide;
+
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 
 
-public class SmokeSteps {
+public class crearSolicitudSteps {
 
-    @Given("^ingreso al juego$")
-    public void ingreso_al_juego() throws Throwable {
-        Selenide.open("http://localhost:4567/");
-    }
+	@Given("^creo una solicitud$")
+	public void creo_una_solicitud() throws Throwable {
+		Selenide.open("http://localhost:4567/");
+	}
 
-    @Then("^veo el mensaje \"([^\"]*)\"$")
-    public void veo_el_mensaje(String message) throws Throwable {
-        $(By.id("welcome")).shouldHave(text(message));
-    }
+	@When("^seleccione el area que involucra la solucion \"([^\"]*)\"$")
+	public void seleccione_el_area_que_involucra_la_solucion(String area) throws Throwable {
+		$(By.id("area")).setValue(area);
+	}
+
+	@When("^ingrese el titulo de la solicitud \"([^\"]*)\"$")
+	public void ingrese_el_titulo_de_la_solicitud(String titulo) throws Throwable {
+		$(By.id("titulo")).setValue(titulo);
+	}
+
+	@When("^digitar la descripcion de la solicitud \"([^\"]*)\"$")
+	public void digitar_la_descripcion_de_la_solicitud(String descripcion) throws Throwable {
+		$(By.id("descripcion")).setValue(descripcion);
+	}
+
+	@Then("^se registrara el ticket \"([^\"]*)\"$")
+	public void se_generara_un_mensaje_de(String linkId) throws Throwable {
+		 $(By.id(linkId)).click();
+	}
+
 }
